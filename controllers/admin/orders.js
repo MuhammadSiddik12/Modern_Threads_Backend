e047b0dacab635681e0aa732a3d403e897dc38e1
Muhammad Siddik Akabar <siddik20@navgurukul.org>
2024-09-05 06:36:57 +0530

    Add more details in order details api admin

--- controllers/admin/orders.js
+++ controllers/admin/orders.js
@@ -1,9 +1,21 @@
+const { Op } = require("sequelize");
+const Cart = require("../../models/cart");
 const Order = require("../../models/order");
+const Product = require("../../models/product");
+const User = require("../../models/user");
 
 exports.getAllOrders = async (req, res) => {
 	try {
-		// Fetch all users
-		const orders = await Order.findAll();
+		// Fetch all orders
+		const orders = await Order.findAll({
+			include: [
+				{
+					model: User,
+					as: "user_details",
+					attributes: ["user_id", "first_name", "last_name", "email"],
+				},
+			],
+		});
 
 		return res.status(200).json({
 			success: true,
@@ -21,7 +33,7 @@ exports.getAllOrders = async (req, res) => {
 
 exports.getOrderDetails = async (req, res) => {
 	try {
-		const { order_id } = req.query; // Extract user ID from the request query
+		const { order_id } = req.query; // Extract order ID from the request query
 
 		// Validate Inputs
 		if (!order_id) {
@@ -31,22 +43,54 @@ exports.getOrderDetails = async (req, res) => {
 			});
 		}
 
-		// Fetch the user by ID
+
 		const order = await Order.findOne({
-			where: { order_id },
+			where: {
+				order_id: order_id,
+			},
+			include: [
+				{
+					model: User,
+					as: "user_details",
+					attributes: ["user_id", "first_name", "last_name", "email"],
+				},
+			],
 		});
 
 		if (!order) {
-			return res.status(404).json({
+			return res.status(400).json({
 				success: false,
-				message: "User not found.",
+				message: "order details not found.",
 			});
 		}
+
+		// Find all cart items where product_id is in the array of order_items
+		const cartItems = await Cart.findAll({
+			where: {
+				cart_id: {
+					[Op.in]: order.order_items,
+				},
+			},
+			include: [
+				{
+					model: Product,
+					as: "product_details",
+				},
+			],
+		});
+
+		// Integrate cart items into order
+		const ordersWithCart = {
+			...order.toJSON(),
+			cart_items: cartItems.filter((cartItem) =>
+				order.order_items.includes(cartItem.cart_id)
+			),
+		};
 
 		return res.status(200).json({
 			success: true,
 			message: "Order fetched successfully!",
-			data: order,
+			data: ordersWithCart,
 		});
 	} catch (error) {
 		return res.status(500).json({

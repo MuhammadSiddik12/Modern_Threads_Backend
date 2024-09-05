ec744664ca80f7d2f0367ca6e1662a00ee23b52e
Muhammad Siddik Akabar <siddik20@navgurukul.org>
2024-09-05 19:49:32 +0530

    add search and pagination in user listing api

--- controllers/admin/users.js
+++ controllers/admin/users.js
@@ -1,14 +1,45 @@
+const { Op } = require("sequelize");
 const User = require("../../models/user");
 
 exports.getAllUsers = async (req, res) => {
 	try {
 		// Fetch all users
-		const users = await User.findAll();
+
+		const { page, limit, search } = req.query;
+		// Fetch all categories
+
+		const pageNumber = Number(page) || 1;
+		const pageSize = Number(limit) || 10;
+
+		const users = await User.findAll({
+			where: {
+				first_name: {
+					[Op.like]: `%${search}%`, // Search by category name (case insensitive)
+				},
+			},
+			limit: pageSize, // Number of items per page
+			offset: (pageNumber - 1) * pageSize, // Calculate offset for pagination
+		});
+
+		const total_users = await User.findAll({
+			where: {
+				first_name: {
+					[Op.like]: `%${search}%`, // Search by category name (case insensitive)
+				},
+				last_name: {
+					[Op.like]: `%${search}%`, // Search by category name (case insensitive)
+				},
+				email: {
+					[Op.like]: `%${search}%`, // Search by category name (case insensitive)
+				},
+			},
+		});
 
 		return res.status(200).json({
 			success: true,
 			message: "Users fetched successfully!",
 			data: users,
+			total_count: Math.ceil(total_users.length / parseInt(limit, 10)),
 		});
 	} catch (error) {
 		return res.status(500).json({

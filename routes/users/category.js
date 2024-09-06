5e9f27bae9f5bf6fc24cb72d9ad650a74ea46cce
Muhammad Siddik Akabar <siddik20@navgurukul.org>
2024-09-06 19:43:13 +0530

    remove auth from public apis

--- routes/users/category.js
+++ routes/users/category.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../../controllers/users/category");
-const authenticateToken = require("../../middleware/authUserToken");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
 	res.send("respond with a resource");
 });
-router.get(
-	"/getAllCategories",
-	authenticateToken,
-	categoryController.getAllCategories
-);
-router.get(
-	"/getCategoryDetails",
-	authenticateToken,
-	categoryController.getCategoryDetails
-);
+router.get("/getAllCategories", categoryController.getAllCategories);
+router.get("/getCategoryDetails", categoryController.getCategoryDetails);
 
 module.exports = router;
--- routes/users/product.js
+++ routes/users/product.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../../controllers/users/product");
-const authenticateToken = require("../../middleware/authUserToken");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
 	res.send("respond with a resource");
 });
 
-router.get(
-	"/getAllProducts",
-	authenticateToken,
-	productController.getAllProducts
-);
-router.get(
-	"/getProductById",
-	authenticateToken,
-	productController.getProductById
-);
+router.get("/getAllProducts", productController.getAllProducts);
+router.get("/getProductById", productController.getProductById);
 
 module.exports = router;
